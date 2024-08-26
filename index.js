4b756f5dcfdc88ce17f26076b2dd98523d95d827
nitin9753 <nitinkhandelwal5050@gmail.com>
2024-08-26 17:43:48 +0530

    changed server file

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/schools', schoolRoutes);
-
+app.get("/", (req, res)=>{
+    res.send("<h1>School Management Server</h1>");
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
